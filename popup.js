306e4f31e340f2333226b77705561b7090dac5a3
ohenecoker <coker.christian@gmail.com>
2020-06-29 08:56:31 -0400

    automatically alter look on page load

--- popup.js
+++ popup.js
@@ -4,14 +4,9 @@ chrome.storage.sync.get("color", function (data) {
   changeColor.style.backgroundColor = data.color;
   changeColor.setAttribute("value", data.color);
 });
-changeColor.onclick = function (element) {
-  let color = element.target.value;
-  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    chrome.tabs.executeScript(tabs[0].id, {
-      code:
-        'document.querySelector("#trello-root").setAttribute("style", "background-color:' +
-        color +
-        '")',
-    });
+
+chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+  chrome.tabs.executeScript(tabs[0].id, {
+    file: "minimal.js",
   });
-};
+});
